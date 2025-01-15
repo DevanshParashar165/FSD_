ad79371dd84ed61c9855966e5ebbbc0e5de43380
DevanshParashar165 <parashardevansh165@gmail.com>
2025-01-15 12:18:38 +0530

    Typecasting

---
+++ Javascript/Regular_expression/Typecasting/script.js
@@ -0,0 +1,49 @@
+console.log(Number(3.14))
+console.log(Number(Math.PI))
+console.log(Number("Hi"))
+console.log(Number(" "))
+console.log(Number("99 88"))
+console.log(Number("Jonh"))
+
+let y = "John";
+let x = + y;
+console.log(x)
+
+console.log(x.toString())
+console.log((123).toString())
+console.log()
+
+d = new Date()
+
+console.log(d.getTime())
+console.log((d.getTime()).toString())
+
+nd = new Date()
+console.log(Number(nd))
+console.log(String(nd))
+console.log(Date.toString())
+console.log(String(Date))
+
+console.log(Number(false))
+console.log(Number(true))
+
+console.log(String(false))
+console.log(String(true))
+
+console.log(String(0))
+console.log(String(1))
+
+console.log(5 + null)
+console.log("5" + null)
+console.log("5" + 2)
+console.log("5" - 2)
+console.log("5" * "2")
+
+// let myVar={name:"john"};
+// let myVar = [1,2,3,4]
+let myVar = new Date()
+// let myVar = 123
+// let myVar = true
+// let myVar = false
+
+document.getElementById('demo').innerHTML=myVar;
